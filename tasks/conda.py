93d0984630cd0f3d043a972685b27c511280baca
pierricC <37798335+pierricC@users.noreply.github.com>
2021-10-04 21:53:38 +0200

    Create conda.py

---
+++ tasks/conda.py
@@ -0,0 +1,25 @@
+"""Conda tasks."""
+
+import logging
+
+from invoke import task
+
+logger = logging.getLogger(__name__)
+
+
+@task
+def create(c):
+    """Recreate the conda environment."""
+    logger.info("Creating conda environment vx-lyo-dt")
+    c.run("conda-merge environment.run.yml environment.dev.yml > environment.yml")
+    c.run("mamba env create --force", pty=True)
+    c.run("rm environment.yml", pty=True)
+
+
+@task
+def update(c):
+    """Update the conda environment."""
+    logger.info("Updating conda environment vx-lyo-dt")
+    c.run("conda-merge environment.run.yml environment.dev.yml > environment.yml")
+    c.run("mamba env update --prune", pty=True)
+    c.run("rm environment.yml", pty=True)
