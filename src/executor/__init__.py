f91f1d9356db52cb047285306055046b1ff17161
pierricC <37798335+pierricC@users.noreply.github.com>
2021-10-04 22:00:58 +0200

    Create __init__.py

---
+++ src/executor/__init__.py
@@ -0,0 +1,8 @@
+"""
+Training / Inference with GPU/CPU.
+
+Contains functions and scripts that train the model
+or use it to predict something in diffenent env
+like executors for GPUs, executors for distributed systems
+This package is the connection with the outer world.
+"""
